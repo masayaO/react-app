2b93df1db011fdcee6ed1fe75daccbf0b7bbe10e
Masaya Okuda <okuda4645@gmail.com>
2021-01-07 22:44:22 +0900

    add errorMessages

---
+++ item-manager/src/firebase/error-messages.ts
@@ -0,0 +1,8 @@
+const errorMessages = {
+  'auth/email-already-exists':
+    'メールアドレスはすでに既存のユーザーによって使用されています',
+  'auth/invalid-password':
+    'password ユーザー プロパティに指定された値は無効です。6文字以上の文字列を指定する必要があります',
+};
+
+export default errorMessages;
